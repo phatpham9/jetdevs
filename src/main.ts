40d7da002e9af16cc43a4ab6413cb94ae94ce99e
Phat Pham <phat@onroads.xyz>
2024-05-15 21:39:38 +0700

    feat(closes #3): add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('jetdevs')
+    .setDescription('Blog API Service')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: true,
+  });
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
